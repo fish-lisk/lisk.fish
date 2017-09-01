bb52d58edb9af36c73c877e9e2b40480600ad72e
George Reith <george@marvelapp.com>
2017-09-02 00:40:23 +0100

    Setup linting

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    root: true,
+    extends: 'airbnb',
+    rules: {
+        indent: ['error', 4, { SwitchCase: 1, VariableDeclarator: 1 }],
+        'max-len': 'off',
+    },
+};
---
+++ index.js
@@ -0,0 +1,8 @@
+// import lisk from 'lisk-js';
+//
+// const options = {
+//     randomPeer: true,
+//     testnet: true, // Default false. Set to true to use the testnet. Set to false to use the mainnet.
+// };
+//
+// const liskApi = lisk.api(options);
