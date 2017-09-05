8f277322259d3ff31161b674f8e1811dd0ab7f94
George Reith <george@marvelapp.com>
2017-09-05 03:16:20 +0100

    server error logging

--- packages/server/index.js
+++ packages/server/index.js
@@ -2,16 +2,52 @@
 require('dotenv').config({ path: '../../.env' });
 
 const Hapi = require('hapi');
+const good = require('good');
+const path = require('path');
 
 const server = new Hapi.Server();
 const connection = server.connection({
     host: '0.0.0.0',
     port: process.env.PORT,
 });
-server.start((err) => {
-    if (err) throw err;
-    console.log(`Server started at: ${server.info.uri}`);
-});
+
+const goodOptions = {
+    ops: {
+        interval: 1000,
+    },
+    reporters: {
+        fileReporter: [
+            {
+                module: 'good-squeeze',
+                name: 'Squeeze',
+                args: [{ error: '*' }],
+            },
+            {
+                module: 'good-squeeze',
+                name: 'SafeJson',
+            },
+            {
+                module: 'good-file',
+                args: [path.join(__dirname, 'error.log')],
+            },
+        ],
+    },
+};
+
+server.register(
+    {
+        register: good,
+        options: goodOptions,
+    },
+    (pluginErr) => {
+        if (pluginErr) throw pluginErr;
+
+        server.start((serverErr) => {
+            if (serverErr) throw serverErr;
+            console.info(`Server started at: ${server.info.uri}`);
+        });
+    },
+);
 
 module.exports = {
     server,
