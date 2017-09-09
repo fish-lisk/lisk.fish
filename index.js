43c0c2b53e12202871b8bafb237a65bda3e5c87e
George Reith <george@marvelapp.com>
2017-09-09 12:38:08 +0100

    Add newrelic

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Set up environment variables
 require('dotenv').config({ path: '.env' });
+// eslint-disable-next-line global-require
+if (process.env.NEW_RELIC_API_KEY) require('newrelic');
 
 require('@lisk.fish/api');
 require('@lisk.fish/client');
