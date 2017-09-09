99f6ac9fca396d499be22e5c4ee5f50eea50b3a2
George Reith <george@marvelapp.com>
2017-09-09 14:16:33 +0100

    Fix wrong name for new relic license env variable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Set up environment variables
 require('dotenv').config({ path: '.env' });
 // eslint-disable-next-line global-require
-if (process.env.NEW_RELIC_API_KEY) require('newrelic');
+if (process.env.NEW_RELIC_LICENSE_KEY) require('newrelic');
 
 require('@lisk.fish/api');
 require('@lisk.fish/client');
